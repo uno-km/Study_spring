da45f484678457f5ebf1826cd5bb88d2caf7d7f6
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-09-19 23:21:21 +0900

    뭐가문제지?

--- spring/src/main/java/org/spring/controller/HomeController.java
+++ spring/src/main/java/org/spring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.spring.controller;
+package org.spring.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
---
+++ spring/src/main/java/org/spring/sample/Chef.java
@@ -0,0 +1,11 @@
+package org.spring.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+public class Chef {
+
+}
