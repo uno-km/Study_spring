4389f65bd2f3a0513f0f2e49f266312ce35870ec
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-09-21 00:06:15 +0900

    왜 다불러오지..?

--- Study_spring/src/test/java/org/study/mapper/BoardMapperTests.java
+++ Study_spring/src/test/java/org/study/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.study.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -14,11 +15,46 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class BoardMapperTests {
 
-	@Setter(onMethod_ = { @Autowired })
+	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
 
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+
+		mapper.insert(board);
+
+		log.info(board);
+	}
+
+	@Test
+	public void testInertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+
+		mapper.insertSelectKey(board);
+
+		log.info(board);
+	}
+
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}
+
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT : " + mapper.delete(3L));
 	}
 }
