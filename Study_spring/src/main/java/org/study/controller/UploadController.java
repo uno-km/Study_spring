0025fb051dd4cfc916ae72e71fc2f2f8b86bebd6
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-13 18:26:38 +0900

    썸네일 모두 정상작동

--- Study_spring/src/main/java/org/study/controller/UploadController.java
+++ Study_spring/src/main/java/org/study/controller/UploadController.java
@@ -10,11 +10,13 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -141,5 +143,29 @@ public class UploadController {
 
 		} // end for
 		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) {
+
+		log.info("fileName: " + fileName);
+
+		File file = new File("C:\\programing\\upload\\" + fileName);
+
+		log.info("file: " + file);
+
+		ResponseEntity<byte[]> result = null;
+
+		try {
+			HttpHeaders header = new HttpHeaders();
+
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
 	}
 }
