624cf17c6ec76aff6136f6dee1a9b0a16e1c38e7
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-14 14:09:53 +0900

    첨부파일을 위한 기존 서버단 설정완료

---
+++ Study_spring/src/main/java/org/study/mapper/BoardAttachMapper.java
@@ -0,0 +1,19 @@
+package org.study.mapper;
+
+import java.util.List;
+
+import org.study.domain.BoardAttachVO;
+
+public interface BoardAttachMapper {
+
+	public void insert(BoardAttachVO vo);
+
+	public void delete(String uuid);
+
+	public List<BoardAttachVO> findByBno(Long bno);
+
+	public void deleteAll(Long bno);
+
+	public List<BoardAttachVO> getOldFiles();
+
+}
