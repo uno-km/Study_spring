8ef04b19ff6b98edd9d19fdf762446770033b4ae
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-10-18 15:47:50 +0900

    왜 jsp파일을 못읽지?

--- Study_spring/src/main/java/org/study/controller/SampleController.java
+++ Study_spring/src/main/java/org/study/controller/SampleController.java
@@ -95,20 +95,21 @@ public class SampleController {
 
 	@GetMapping("/all")
 	public void doAll() {
-
 		log.info("do all can access everybody");
 	}
 
 	@GetMapping("/member")
 	public void doMember() {
-
 		log.info("logined member");
 	}
 
 	@GetMapping("/admin")
 	public void doAdmin() {
-
 		log.info("admin only");
+	}
+	@GetMapping("/uno")
+	public void doUno() {
+		log.info("uno only");
 	}
 
 }
